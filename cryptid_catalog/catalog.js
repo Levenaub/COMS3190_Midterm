1c8de162c6e59dbe4bbbb618f895a6d25ea4ad61
JbhHennings <jbh.hennings@gmail.com>
2024-10-16 18:16:22 -0500

    Cryptid catalog complete

---
+++ cryptid_catalog/catalog.js
@@ -0,0 +1,39 @@
+fetch('catalog.json')
+  .then(response => response.json())
+  .then(data => {
+
+    const cryptids = data.cryptids;
+    const container = document.getElementById('content-container');
+
+    let rowHTML = '';  
+    cryptids.forEach((page, index) => {
+      
+      if (index % 3 === 0) {
+        if (index !== 0) {
+          rowHTML += '</div>';
+        }
+        rowHTML += '<div class="row">';
+      }
+      
+      rowHTML += `
+        <div class="col-md-4 mb-4">
+          <div class="card">
+            <div style="text-align: center">
+              <img src="${page.image}" alt="${page.name}">
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">${page.name}</h5>
+              <p class="card-text">${page.description}</p>
+            </div>
+          </div>
+        </div>
+      `;
+      
+      if (index === cryptids.length - 1) {
+        rowHTML += '</div>';
+      }
+    });
+
+    container.innerHTML = rowHTML;
+  })
+  .catch(error => console.error('Error loading content:', error));
